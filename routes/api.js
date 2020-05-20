db2f4f7d9809d6412ce63140dd439a56fc477713
Mai <nawaphat2563@gmail.com>
2020-05-20 12:47:07 +0700

    GET Requests

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,12 @@ const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers')
 
+//GET, POST, PUT, DELETE
+
 router.get('/:resource',(req , res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
+	const filters = req.query
 
 	if(controller == null){
 		res.json({
@@ -15,7 +18,35 @@ router.get('/:resource',(req , res) => {
 		return
 	}
 
-	controller.get()
+	controller.get(filters)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: data
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+router.get('/:resource/:id', (req,res) => {
+	const resource = req.params.resource
+	const id = req.params.id
+
+	const controller = controllers[resource]
+	if (controller == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Invalid Resource'
+		})
+		return
+	}
+
+	controller.getById(id)
 	.then(data => {
 		res.json({
 			confirmation: 'success',
